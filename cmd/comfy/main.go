fc71b24ba9a6efeba8e51bec8adf880725e9be7f
rhoninn11 <rhoninn11@gmail.com>
2025-04-24 18:50:31 +0200

    - starter prompts for comfy

--- cmd/comfy/main.go
+++ cmd/comfy/main.go
@@ -8,4 +8,11 @@ func main() {
 	todoMeassage := "TODO: here will be implemented client calling comfyui over grpc\n" +
 		"Maybe explor posibility of usage jsonrpc, or sending proto buffers over websocket"
 	fmt.Println(todoMeassage)
+
+	//TODO: parę zdjęć można by tu wygenerować
+
+	prompts := []string{
+		"warrior wakes up early morning, in his friend house after yesterdey's happy bonfire day:D",
+		"mage wakes up erlier so he can go fishing with uncle bob",
+	}
 }
