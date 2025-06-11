2650b7e6b7835e1e0e965716bf1d521e152e03cf
rhoninn11 <rhoninn11@gmail.com>
2025-06-11 19:06:40 +0200

    - rpc parsing

--- cmd/reflect/main.go
+++ cmd/reflect/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"log"
 	"mea_go/internal"
@@ -10,6 +9,14 @@ import (
 	"strings"
 )
 
+type RpcDesc struct {
+	Fn     string
+	InTpy  string
+	OutTpy string
+}
+type ProtoTypes = []string
+type RpcDescs = []RpcDesc
+
 func parseProto(filePath string) error {
 	content, err := os.ReadFile(filePath)
 	if err != nil {
@@ -17,61 +24,57 @@ func parseProto(filePath string) error {
 	}
 
 	fmt.Println("+++ listing content of ", filePath)
-	err = protoTypes(content)
+	protos, err := protoTypes(content)
 	if err != nil {
 		return fmt.Errorf("!!! type extraction failed, %v", err)
 	}
-	err = protoRpcs(content)
+	rpcs, err := protoRpcs(content)
 	if err != nil {
 		return fmt.Errorf("!!! rpcs extraction failed, %v", err)
 	}
+	_ = protos
+	_ = rpcs
+	fmt.Println(protos)
+	fmt.Println(rpcs)
 	return nil
 }
 
-func protoTypes(content []byte) error {
-	lines := bytes.Split(content, []byte("\n"))
-
-	var types [][][]byte = make([][][]byte, 0, 64)
-	var start int
-	var end int
-	var opened bool = false
-	for i, line := range lines {
-		a := bytes.Contains(line, []byte("message"))
-		b := bytes.Contains(line, []byte("{"))
-		c := bytes.Contains(line, []byte("}"))
-
-		if a && b && !opened {
-			start = i
-			opened = true
-		}
-
-		if c && opened {
-			end = i + 1
-			types = append(types, lines[start:end])
-			opened = false
-		}
-	}
-
+func protoTypes(content []byte) (ProtoTypes, error) {
 	reRule := `message\s(\w+)\s{`
 	protoRe, err := regexp.Compile(reRule)
 	if err != nil {
-		return fmt.Errorf("!!! fauled to compile re, %v", err)
+		return nil, fmt.Errorf("!!! fauled to compile re, %v", err)
 	}
 
-	for _, tpy := range types {
-		whole := bytes.Join(tpy, []byte("\n"))
-		found := protoRe.FindStringSubmatch(string(whole))
-		if len(found) > 1 {
-			typeName := found[1]
-			fmt.Printf("---found type: %s\n", typeName)
-		}
+	reResult := protoRe.FindAllSubmatch(content, -1)
+	typeSlice := make(ProtoTypes, len(reResult))
+	for i, proto := range reResult {
+		typeSlice[i] = string(proto[1])
 	}
 
-	return nil
+	return typeSlice, nil
 }
 
-func protoRpcs(content []byte) error {
-	return nil
+func protoRpcs(content []byte) (RpcDescs, error) {
+	rpcRule := `rpc\s(\w+)\((\w+)\)\sreturns\s\((\w+)\);`
+	rpcRe, err := regexp.Compile(rpcRule)
+	if err != nil {
+		return nil, fmt.Errorf("!!! rpc rule compile failed, %v", err.Error())
+	}
+
+	rpcSearch := rpcRe.FindAllSubmatch(content, -1)
+	rpcMethods := make([]RpcDesc, len(rpcSearch))
+	for i, single := range rpcSearch {
+		if len(single) < 4 {
+			continue
+		}
+		rpcMethods[i] = RpcDesc{
+			Fn:     string(single[1]),
+			InTpy:  string(single[2]),
+			OutTpy: string(single[3]),
+		}
+	}
+	return rpcMethods, nil
 }
 
 func main() {
